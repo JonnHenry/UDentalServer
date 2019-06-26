eec71f05d750e0e6eb61cba394b642faad14c651
JonnHenry <jonnathanca9601@gmail.com>
2019-06-26 11:13:55 -0500

    Agregación de la ruta de /insumos

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 var equiposApi = require('./Equipos_api').initEquipos;
+var insumosApi = require('./Insumos_api').initInsumos
 
 /*
      conexionBD: conexionBD,
@@ -8,10 +9,15 @@ var equiposApi = require('./Equipos_api').initEquipos;
 function inicialiceRouter(instanciaBD, app) {
     return new Promise((resolve, reject) => {
         try {
-            app.use('equipos', equiposApi({
+            app.use('/equipos', equiposApi({
                 conexionBD: instanciaBD.conexionBD,
                 Productos: instanciaBD.models.Productos,
                 Equipos: instanciaBD.models.Equipos
+            }));
+            app.use('/insumos', insumosApi({
+                conexionBD: instanciaBD.conexionBD,
+                Productos: instanciaBD.models.Productos,
+                Insumos: instanciaBD.models.Insumos
             }));
             resolve(true);
         } catch (error) {
