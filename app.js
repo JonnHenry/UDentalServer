c8d494856807eb4042c79d3a1d5d3e3232e54745
JonnHenry <jonnathanca9601@gmail.com>
2019-07-01 00:56:00 -0500

    Cambio de puerto

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-const port = 3000; //Puerto donde va a funcionar express.js
+const port = 8080; //Puerto donde va a funcionar express.js
 var cors = require('cors');
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({
