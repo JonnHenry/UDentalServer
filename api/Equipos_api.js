48c15ef8eef975fa75a3fff6bf14bf0da3fdef25
JonnHenry <jonnathanca9601@gmail.com>
2019-07-02 12:46:14 -0500

    Creación del metodo de busqueda

--- api/Equipos_api.js
+++ api/Equipos_api.js
@@ -43,114 +43,152 @@ function initEquipos(instanciaBD) {
 
     //Obtener los equipos con 
     router.get('/find', (req, res) => {
-        createQuery(req.body).then(query => {
-            return conexion.transaction(t => {
-                    return conexion.query(query, {
-                            transaction: t,
-                            limit: 1,
-                            lock: true,
-                            raw: true
-                        })
-                        .then(([results, metadata]) => {
-                            res.status(200);
-                            return ({
-                                "error": false,
-                                "data": results
-                            });
-                        })
-                        .catch(err => {
-                            res.status(500);
-                            return ({
-                                message: 'Error, vuelva a intentarlo.',
-                                inserted: false
-                            })
-                        })
+        createQuery(req.body.colAndValueSearch).then(querySearch => {
+            return conexion.query(querySearch)
+                .then(([results, metadata]) => {
+                    res.json ({
+                        "error": false,
+                        "message": "Se econtraron valores para los criterios de busqueda",
+                        "data": results
+                    }).status(200);
                 })
-                .then(result => {
-                    res.json(result)
-                });
+                .catch(err => {                  
+                    res.json ({
+                        "message": 'Error, vuelva a intentarlo y revise ingresados.',
+                        "error": true,
+                        "data": []
+                    }).status(500)
+                })
         })
-    });
+});
 
-
-    //Crear un nuevo equipo
-
-
-
-
-
-    router.put('/update/all/:id', (req, res) => {
+router.put('/update/:id', (req, res) => {
+    try {
         return conexion.transaction(t => {
-            return Equipos.update({
-                    marca: req.body.marca,
-                    observacion: req.body.observacion,
-                    estado: req.body.estado,
-                    stock: req.body.stock
-                }, {
+            return Equipos.update(req.body.dataUpdate, {
+                where: {
+                    id_producto: req.params.id
+                },
+                transaction: t,
+                limit: 1,
+                lock: true
+            }).then(() => {
+                return Productos.update(req.body.dataUpdate, {
                     where: {
-                        id_producto: req.params.id
+                        id: req.params.id
                     },
                     transaction: t,
                     limit: 1,
                     lock: true
                 }).then(() => {
-                    return Productos.update({
-                            nombre: req.body.nombre,
-                            descripcion: req.body.descripcion,
-                            precio_unitario: req.body.precio_unitario
-                        }, {
-                            where: {
-                                id: req.params.id
-                            },
-                            transaction: t,
-                            limit: 1,
-                            lock: true
-                        }).then(() => {
-                            res.status(200);
-                            return ({
-                                'message': 'El Equipo fue actualizado, de manera correcta',
-                                'updated': true
-                            })
-                        })
-                        .catch(() => {
-                            res.status(500);
-                            return ({
-                                'message': 'El Equipo no fue actualizado, vuelva a intentarlo',
-                                'updated': false
-                            })
-                        });
-                })
-                .catch(() => {
-                    res.status(500);
+                    res.status(200);
                     return ({
-                        'message': 'El Equipo no fue actualizado, vuelva a intentarlo',
-                        'updated': false
+                        'message': 'El Equipo fue actualizado, de manera correcta',
+                        'updated': true
                     })
-                });
-
-        }).then(result => {
-            res.json(result)
+                })
+            })
         });
-    })
+    } catch (error) {
+        return ({
+            'message': 'El Equipo no fue actualizado revise los valores ingresados, vuelva a intentarlo',
+            'updated': false
+        })
+    }
+});
 
-    return router
+
+
+router.put('/update/all/:id', (req, res) => {
+    return conexion.transaction(t => {
+        return Equipos.update({
+                marca: req.body.marca,
+                observacion: req.body.observacion,
+                estado: req.body.estado,
+                stock: req.body.stock
+            }, {
+                where: {
+                    id_producto: req.params.id
+                },
+                transaction: t,
+                limit: 1,
+                lock: true
+            }).then(() => {
+                return Productos.update({
+                        nombre: req.body.nombre,
+                        descripcion: req.body.descripcion,
+                        precio_unitario: req.body.precio_unitario
+                    }, {
+                        where: {
+                            id: req.params.id
+                        },
+                        transaction: t,
+                        limit: 1,
+                        lock: true
+                    }).then(() => {
+                        res.status(200);
+                        return ({
+                            'message': 'El Equipo fue actualizado, de manera correcta',
+                            'updated': true
+                        })
+                    })
+                    .catch(() => {
+                        res.status(500);
+                        return ({
+                            'message': 'El Equipo no fue actualizado, vuelva a intentarlo',
+                            'updated': false
+                        })
+                    });
+            })
+            .catch(() => {
+                res.status(500);
+                return ({
+                    'message': 'El Equipo no fue actualizado, vuelva a intentarlo',
+                    'updated': false
+                })
+            });
+
+    }).then(result => {
+        res.json(result)
+    });
+})
+
+return router
 }
-
 
 
 function createQuery(parametros) {
+    var query = 'SELECT id, nombre, descripcion, observacion, precio_unitario, stock, marca, estado, fecha_creacion, fecha_actualizacion FROM equipos JOIN productos ON productos.id = equipos.id_producto';
+    var cont = 0;
+    const cantArg = parametros.length;
     return new Promise((resolve, reject) => {
-        if (parametros.length != 0) {
-            query = 'SELECT id, nombre, descripcion, observacion, precio_unitario, stock, marca, estado, fecha_creacion, fecha_actualizacion FROM equipos JOIN productos ON productos.id = equipos.id_producto WHERE '
+        if (cantArg != 0) {
+            var enteros = ['stock', 'id', 'precio_unitario'];
+            query = query + ' WHERE '
             parametros.forEach(element => {
-                query = query + element + ' AND '
+                var parametros = element.split('=');
+                if (cantArg - 1 > cont) {
+                    var parametros = element.split('=');
+                    if (!enteros.includes(parametros[0])) {
+                        query = query + parametros[0] + "='" + parametros[1] + "' AND ";
+                        cont++;
+                    } else {
+                        query = query + element + ' AND ';
+                        cont++;
+                    }
+                } else {
+                    var parametros = element.split('=');
+                    if (!enteros.includes(parametros[0])) {
+                        query = query + parametros[0] + "='" + parametros[1] + "'";
+                    } else {
+                        query = query + element + ' ';
+                    }
+                }
             });
-            console.log(query);
         }
         resolve(query);
-        reject(query);
+        reject(query)
     })
 }
-
 
 module.exports.initEquipos = initEquipos;
