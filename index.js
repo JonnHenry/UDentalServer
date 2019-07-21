ba67a9f8f294c509601cfd2a14e3808fa602f836
JonnHenry <jonnathanca9601@gmail.com>
2019-07-21 16:56:36 -0500

    Metodo de autenticación de usuarios agregado

--- index.js
+++ index.js
@@ -20,5 +20,23 @@ conexion('postgres://vemqjijm:0j1siCgvszEiwg6E4LLBjELw-yw3qWPx@raja.db.elephants
     console.log(error);
   })
 });
+
+
+app.post('login',(req,res)=>{
+  conexion('prueba', req.body.user,req.body.password,'localhost').then(result => {
+    apiUse(result, app).then(conect => {
+      if (conect) {
+        console.log('La conexión es correcta');
+      } else {
+        console.log('La conexión no es correcta');
+      }
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+  });
+})
+
+
 // conexionBD Me trae la conexión y los modelos de la base de datos
 
